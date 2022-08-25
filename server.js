60574d0ed5a42a47caee68e24a4cb0186bdd3abb
ardayasar <erddoerdo12@gmail.com>
2022-08-25 14:08:59 +0300

    byte64 add

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express');
+var fs = require('fs');
 const app = express();
-const port = 5200;
 
+const port = 5200;
 app.use(express.json());
+
+var byte64_location = './data/byte64.json';
 
 var pages = {
     main: "/",
@@ -28,6 +31,13 @@ app.post(pages.detected_object, (req, res) => {
 
 app.post(pages.live_image, (req, res) => {
     console.log(req.body);
+
+    var data = fs.readFileSync(byte64_location);
+    let images = JSON.parse(data);
+    images.push(req.body);
+    data = JSON.stringify(images);
+    fs.writeFileSync(byte64_location, data, 'utf-8');
+    
     res.send(req.body);
 })
 
